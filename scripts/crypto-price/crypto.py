e7990e0dd3a583626622d98cf4adedeccc3ee3fc
Kostaintyn Naddur <91757099+naddurkostia@users.noreply.github.com>
2022-07-04 20:55:11 +0300

    crypto

--- scripts/crypto-price/crypto.py
+++ scripts/crypto-price/crypto.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
 
-# Import libraries
 import json
 import requests
   
-# Defining Binance API URL
 key = "https://api.binance.com/api/v3/ticker/price?symbol="
   
 # Making list for multiple crypto's
 currencies = ["BTCUSDT", "DOGEUSDT", "LTCUSDT", "ETHUSDT", "SOLUSDT", "TRXUSDT", "BNBUSDT"]
 j = 0
   
-# running loop to print all crypto prices
 for i in currencies:
-    
-    # completing API for request
+
     url = key+currencies[j]  
     data = requests.get(url)
     data = data.json()
